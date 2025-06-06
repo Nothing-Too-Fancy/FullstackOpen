4d654d52cbe8ed5dfa0206bf6b68956f8b3b54fe
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-05 21:45:35 -0700

    exercise 2.11

--- Exercises/part2/phonebook/src/App.jsx
+++ Exercises/part2/phonebook/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter.jsx'
 import PersonForm from './components/PersonForm.jsx'
 import Phonebook from './components/Phonebook.jsx'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
@@ -17,6 +14,14 @@ const App = () => {
   const isDuplicate = () => {
     return persons.find((person) => person.name === newName) ? !undefined : false
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  })
 
   const addNumber = (event) => {
     event.preventDefault()
