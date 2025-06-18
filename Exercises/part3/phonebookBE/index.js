0173bcb73b1fb616d1062b2eab3c8edc32f6a084
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-17 17:45:37 -0700

    exercise 3.2

--- Exercises/part3/phonebookBE/index.js
+++ Exercises/part3/phonebookBE/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const receivedAt = new Date()
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+     <p>${receivedAt}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
