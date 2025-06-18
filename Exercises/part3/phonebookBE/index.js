fcf3897edf1344d6f10571830210bc01324aed67
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-17 17:55:40 -0700

    exercise 3.3

--- Exercises/part3/phonebookBE/index.js
+++ Exercises/part3/phonebookBE/index.js
@@ -35,6 +35,16 @@ app.get('/info', (request, response) => {
      <p>${receivedAt}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find((person) => person.id === id)
+
+  if(person)
+    response.send(person)
+  else
+    response.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
