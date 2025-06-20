8afdf3841d683607550d35296cd3da728b9e43e1
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-20 15:26:05 -0700

    serve the frontend production files to the backend to show static page on server

--- Exercises/part3/phonebookBE/index.js
+++ Exercises/part3/phonebookBE/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(dist))
 app.use(morgan('tiny'))
 
 let persons = [
@@ -79,7 +80,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
