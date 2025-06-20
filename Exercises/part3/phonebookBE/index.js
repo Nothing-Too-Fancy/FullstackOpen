3001d1026f7a810d8b6da7e4ec82364e704cacf0
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-20 15:33:48 -0700

    removed cors middleware

--- Exercises/part3/phonebookBE/index.js
+++ Exercises/part3/phonebookBE/index.js
@@ -1,9 +1,9 @@
-const cors = require('cors')
+
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(cors())
+
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('tiny'))
