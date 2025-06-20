2f7ad55654792bd18fd5df6a6c07649b88a713a3
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-20 14:48:36 -0700

    exercise 3.9

--- Exercises/part2/phonebook/src/services/persons.js
+++ Exercises/part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -22,9 +22,9 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll, 
-  create, 
+export default {
+  getAll,
+  create,
   remove,
   update
 }
--- Exercises/part3/phonebookBE/index.js
+++ Exercises/part3/phonebookBE/index.js
@@ -1,31 +1,33 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (request, response) => {
@@ -43,24 +45,24 @@ app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find((person) => person.id === id)
 
-  if(person)
+  if (person)
     response.send(person)
   else
     response.status(404).end()
 })
 
 app.post('/api/persons', (request, response) => {
-  const {id, name, number} = request.body
+  const { id, name, number } = request.body
   const person = request.body
-  const maxID = persons.length > 0 
-    ? Math.max(...persons.map(n => Number(n.id))) 
+  const maxID = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
     : 0
 
-  if(name === "" || number === "") {
-    return response.status(400).json({error: 'Missing name or number'})
+  if (name === "" || number === "") {
+    return response.status(400).json({ error: 'Missing name or number' })
   }
-  if(persons.find(x => x.name === name)) {
-    return response.status(400).json({error: 'Name already exists in phonebook'})
+  if (persons.find(x => x.name === name)) {
+    return response.status(400).json({ error: 'Name already exists in phonebook' })
   }
 
   person.id = String(maxID + 1)
@@ -72,7 +74,7 @@ morgan.token('content', function (req, res) { return req.body })
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  persons= persons.filter((person) => person.id !== id)
+  persons = persons.filter((person) => person.id !== id)
 
   response.status(204).end()
 })
