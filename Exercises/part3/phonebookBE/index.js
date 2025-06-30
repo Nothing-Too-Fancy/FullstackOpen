7c496495097a4927f3bd472617397ce437803d13
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-06-30 13:16:28 -0700

    uibuild

--- Exercises/part3/phonebookBE/index.js
+++ Exercises/part3/phonebookBE/index.js
@@ -1,38 +1,19 @@
 
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
+const Person = require('./models/person')
 
+const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan('tiny'))
 
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -43,33 +24,37 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find((person) => person.id === id)
-
-  if (person)
-    response.send(person)
-  else
-    response.status(404).end()
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person)
+        response.json(person)
+      else
+        response.status(404).end()
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    }
+    )
 })
 
 app.post('/api/persons', (request, response) => {
   const { id, name, number } = request.body
-  const person = request.body
-  const maxID = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
 
   if (name === "" || number === "") {
     return response.status(400).json({ error: 'Missing name or number' })
   }
-  if (persons.find(x => x.name === name)) {
-    return response.status(400).json({ error: 'Name already exists in phonebook' })
-  }
 
-  person.id = String(maxID + 1)
-  persons = persons.concat(person)
-  console.log(person)
-  response.json(person)
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(savedPerson => {
+    console.log(person)
+    response.json(person)
+  })
+
 })
 morgan.token('content', function (req, res) { return req.body })
 
@@ -80,7 +65,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
