da07aa13a4742f7cfd210e3ce2b2d4e4c0892f31
Ed <47069204+Nothing-Too-Fancy@users.noreply.github.com>
2025-07-01 15:24:09 -0700

    refactored backend directory structure

--- Exercises/part2/phonebook/src/components/Phonebook.jsx
+++ Exercises/part2/phonebook/src/components/Phonebook.jsx
@@ -2,11 +2,11 @@ import Person from "./Person"
 import personService from '../services/persons'
 
 const deletePerson = (id, setPersons, persons) => {
-  if(window.confirm(`Are you sure you want to delete ${persons.find(x => x.id === id).name}?`))
+  if (window.confirm(`Are you sure you want to delete ${persons.find(x => x.id === id).name}?`))
     personService.remove(id)
-      .then(data => 
+      .then(() =>
         setPersons(persons.filter(x => x.id !== id))
-    )
+      )
 }
 
 
@@ -14,17 +14,17 @@ const Phonebook = (props) => {
 
   return (
     <>
-    {props.persons
-      .filter((person) => 
-        person.name.toLowerCase()
-          .includes(props.filter.toLowerCase()))
-            .map((person) => 
-              <Person 
-                key={person.name}
-                name={person.name}
-                number={person.number} 
-                deleteCallback={() => deletePerson(person.id, props.setPersons, props.persons)}
-              />)}
+      {props.persons
+        .filter((person) =>
+          person.name.toLowerCase()
+            .includes(props.filter.toLowerCase()))
+        .map((person) =>
+          <Person
+            key={person.name}
+            name={person.name}
+            number={person.number}
+            deleteCallback={() => deletePerson(person.id, props.setPersons, props.persons)}
+          />)}
     </>
   )
 }
